68875a29f881a9ee8d5c024d18eca9fa213a8650
Viktor Lazarev <vaksick@gmail.com.ua>
2024-06-27 15:13:54 +0300

    fix

--- cmake/cpplog.cmake
+++ cmake/cpplog.cmake
@@ -1,18 +1,15 @@
 set(DISABLE_UNINSTALL_TARGET TRUE)
 	# TODO disabled for now, since cpplog uninstall target conflicts with VC4C uninstall target
 	include(FetchContent)
-	FetchContent_Declare(cpplog GIT_REPOSITORY https://github.com/doe300/cpplog.git GIT_TAG v0.6)
+	FetchContent_Declare(cpplog GIT_REPOSITORY https://github.com/doe300/cpplog.git GIT_TAG master)
 	FetchContent_GetProperties(cpplog)
 	if(NOT cpplog_POPULATED)
-	    FetchContent_Populate(cpplog)
-	
-	    # Примените патч
-	    execute_process(
-	        COMMAND git apply ${CMAKE_SOURCE_DIR}/cmake/disable_uninstall.patch
-	        WORKING_DIRECTORY ${cpplog_SOURCE_DIR}
-	    )
-	
-	    add_subdirectory(${cpplog_SOURCE_DIR} ${cpplog_BINARY_DIR})
+		FetchContent_Populate(cpplog)
+		execute_process(
+			COMMAND git apply ${CMAKE_SOURCE_DIR}/cmake/disable_uninstall.patch
+			WORKING_DIRECTORY ${cpplog_SOURCE_DIR}
+		)
+		add_subdirectory(${cpplog_SOURCE_DIR} ${cpplog_BINARY_DIR})
 	endif()
 	# CMake configuration flags for cpplog project
 	set(CPPLOG_NAMESPACE logging)
